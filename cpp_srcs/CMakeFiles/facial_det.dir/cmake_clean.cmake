1ae8110456d2792933c838035e496bc47352c28d
jpinyot <pep.pinyot@gmail.com>
2018-10-21 13:53:14 +0200

    first c++ commit, frontal face detection

---
+++ cpp_srcs/CMakeFiles/facial_det.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/facial_det.dir/srcs/facial_detection2.cpp.o"
+  "facial_det.pdb"
+  "facial_det"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/facial_det.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
