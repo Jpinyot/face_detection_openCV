2a16f3cc4ddc22d4cafa699affaadde767d3b886
jpinyot <pep.pinyot@gmail.com>
2018-10-24 16:11:56 +0200

    facial detecion correct

--- cpp_srcs/srcs/facial_detection.cpp
+++ cpp_srcs/srcs/facial_detection.cpp
@@ -1,125 +1,54 @@
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
-
 #include <iostream>
 #include <stdio.h>
 
 using namespace std;
 using namespace cv;
 
-/* Function Headers */
-void detectAndDisplay(Mat frame);
-
-/* Global variables */
-/* Copy this file from opencv/data/haarscascades to target folder */
-string face_cascade_name = "data/haarcascades/haarcascade_frontalface_default.xml";
+string face_cascade_name = ".data/haarcascades/haarcascade_frontalface_default.xml";
 CascadeClassifier face_cascade;
-string window_name = "Capture - Face detection";
-int filenumber; /*Number of file to be saved */
-string filename;
+String window_name = "Capture";
 
-/* Function main */
-int main(int ac, char **av)
+void detect_face(Mat frame)
 {
-    /* Load the cascade */
-    if (!face_cascade.load(face_cascade_name) || ac != 2)
-	{
-        printf("--(!)Error loading\n");
-        return (-1);
-    }
-    /* Read the image file */
-    Mat frame = imread(av[1]);
-    /* Apply the classifier to the frame */
-    if (!frame.empty())
-        detectAndDisplay(frame);
-    else
-        printf(" --(!) No captured frame -- Break!");
-    int c = waitKey(2000);
-    if (27 == char(c)) return (0);
-}
-
-// Function detectAndDisplay
-void detectAndDisplay(Mat frame)
-{
-    std::vector<Rect> faces;
-    Mat frame_gray;
-    Mat crop;
-    Mat res;
-    Mat gray;
-    string text;
-    stringstream sstm;
+    Mat				frame_gray;
+    Mat				faceROI;
+    vector<Rect>	faces;
+    vector<Rect>	eyes;
 
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
     equalizeHist(frame_gray, frame_gray);
-
-    // Detect faces
-    face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
-    // Set Region of Interest
-    cv::Rect roi_b;
-    cv::Rect roi_c;
-
-    size_t ic = 0; // ic is index of current element
-    int ac = 0; // ac is area of current element
-
-    size_t ib = 0; // ib is index of biggest element
-    int ab = 0; // ab is area of biggest element
-
-	if (faces.size() <= 0)
-		std::cout << '$';
-    for (ic = 0; ic < faces.size(); ic++) // Iterate through all current elements (detected faces)
+    face_cascade.detectMultiScale(frame_gray, faces);
+    for (size_t i = 0; i < faces.size(); i++)
     {
-        roi_c.x = faces[ic].x;
-        roi_c.y = faces[ic].y;
-        roi_c.width = (faces[ic].width);
-        roi_c.height = (faces[ic].height);
-
-        ac = roi_c.width * roi_c.height; // Get the area of current element (detected face)
-
-        roi_b.x = faces[ib].x;
-        roi_b.y = faces[ib].y;
-        roi_b.width = (faces[ib].width);
-        roi_b.height = (faces[ib].height);
-
-        ab = roi_b.width * roi_b.height; // Get the area of biggest element, at beginning it is same as "current" element
-
-        if (ac > ab)
-        {
-            ib = ic;
-            roi_b.x = faces[ib].x;
-            roi_b.y = faces[ib].y;
-            roi_b.width = (faces[ib].width);
-            roi_b.height = (faces[ib].height);
-        }
-
-        crop = frame(roi_b);
-        resize(crop, res, Size(128, 128), 0, 0, INTER_LINEAR); // This will be needed later while saving images
-        cvtColor(crop, gray, CV_BGR2GRAY); // Convert cropped image to Grayscale
-
-        // Form a filename
-        filename = "";
-        stringstream ssfn;
-        ssfn << filenumber << ".png";
-        filename = ssfn.str();
-        filenumber++;
-
-        imwrite(filename, gray);
-
-        Point pt1(faces[ic].x, faces[ic].y); // Display detected faces on main window - live stream from camera
-        Point pt2((faces[ic].x + faces[ic].height), (faces[ic].y + faces[ic].width));
-        rectangle(frame, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
+        Point center(faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2);
+        ellipse(frame, center, Size(faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar(255, 0, 255), 4);
+        faceROI = frame_gray(faces[i]);
     }
+    imshow("Capture", frame);
+}
 
-    // Show image
-    sstm << "Crop area size: " << roi_b.width << "x" << roi_b.height << " Filename: " << filename;
-    text = sstm.str();
+int main(int ac, const char **av)
+{
+    VideoCapture	capture;
+	Mat				frame;
 
-    putText(frame, text, cvPoint(30, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0, 0, 255), 1, CV_AA);
-    /* imshow("original", frame); */
-
-    if (!crop.empty())
-        imshow("detected", crop);
-    else
-        destroyWindow("detected");
+	if(!face_cascade.load(face_cascade_name))
+	{
+		cout << "Error loading cascade\n";
+		return (-1);
+	}
+	if (ac != 2)
+	{
+		cout << "No valid input";
+		return (-1);
+	}
+	frame = imread(av[1]);
+	if(frame.empty())
+		cout << "No captured frame\n";
+	detect_face(frame);
+	waitKey(2000);
+	return (0);
 }
